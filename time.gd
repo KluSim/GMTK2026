1fde2417e6d409688fc1dc3396888a72aa69b286
Kommunsitus <felix.brandner.1@gmx.de>
2026-07-22 21:19:43 +0200

    feat: layout of shelves ec WIP

--- time.gd
+++ time.gd
@@ -2,25 +2,39 @@ extends Node2D
 
 var lasttime = 0
 @onready var sky = $sky
+
+@export var speed: float = 2.0
+var time: float = -1.0
+var dawn = -1
+var days = 0
+var speed_multiplier = 1         #multiplier how fast the days go
+
+var daytime = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	lasttime =  Time.get_ticks_usec()
+	time = dawn
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var grad: Gradient = sky.texture.gradient
-	var factor: float = (Time.get_ticks_usec()-lasttime)/10000000
-	var yellow := Color(1.0, 0.9, 0.0) # Bright Yellow
-	var blue := Color(0.0, 0.4, 1.0)   # Vivid Blue
-	var color_1 = yellow.lerp(blue, factor)
-	var color_2 = blue.lerp(blue, factor)
+	time += delta * speed /10 / speed_multiplier
 	
-	# Update the two color stops of the gradient (Index 0 and Index 1)
-	grad.set_color(0, color_1)
-	grad.set_color(1, color_2)
-	#if Time.get_ticks_usec() > 10000:
-	#Godot
+	#night 3
+	#morning -2
+	var offset_x: float = (cos(time) )  # Ranges 0.0 to 1.0
+	var offset_y: float = (sin(time) )  # Ranges 0.0 to 1.0
 	
+	# Move the starting point (where the first color begins)
+	if time <= 3*speed_multiplier:
+		sky.texture.fill_from = Vector2(0.5, 1)
+		sky.texture.fill_to = Vector2(1-offset_x, 1-offset_y)
 	pass
+	
+func _input(event):
+	if event.is_action_pressed("nextDay"):
+		time = dawn*speed_multiplier
+		days += 1
+		print("hello")
